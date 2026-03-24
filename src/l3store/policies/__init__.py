2eb536832b3e15cd02860c976427d0994a3b56c1
Tim Makhel <tmaxelhard@gmail.com>
2026-03-24 20:05:13 +0300

    policies: eviction base logic update

--- src/l3store/policies/__init__.py
+++ src/l3store/policies/__init__.py
@@ -1,3 +1,14 @@
 from l3store.policies.base import EvictionPolicy, EvictionDecision
+from l3store.policies.registry import (
+    register_eviction_policy,
+    get_eviction_policy,
+    list_eviction_policies,
+)
 
-__all__ = ["EvictionPolicy", "EvictionDecision"]
+__all__ = [
+    "EvictionPolicy",
+    "EvictionDecision",
+    "register_eviction_policy",
+    "get_eviction_policy",
+    "list_eviction_policies",
+]
